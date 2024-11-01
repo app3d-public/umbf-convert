26fbd332e09cbbd43bfb6467050046be67d73024
Wusiki Jeronii <wusikijeronii@gmail.com>
2024-11-01 06:39:02 +0300

    Update to core: change includes

--- models/jsonbase.cpp
+++ models/jsonbase.cpp
@@ -1,6 +1,6 @@
 #include "jsonbase.hpp"
 #include <assets/asset.hpp>
-#include <core/std/basic_types.hpp>
+#include <astl/basic_types.hpp>
 #include <fstream>
 #include <glm/glm.hpp>
 #include <sstream>
@@ -203,22 +203,23 @@ namespace models
 
     vk::Format parseVkFormat(std::string str)
     {
-        static const astl::map<std::string, vk::Format> formatMap = {{"R8G8B8A8_UNORM", vk::Format::eR8G8B8A8Unorm},
-                                                               {"R8G8B8A8_SNORM", vk::Format::eR8G8B8A8Snorm},
-                                                               {"R8G8B8A8_SRGB", vk::Format::eR8G8B8A8Srgb},
-                                                               {"R8G8B8A8_SINT", vk::Format::eR8G8B8A8Sint},
-                                                               {"R8G8B8A8_UINT", vk::Format::eR8G8B8A8Uint},
-                                                               {"R16G16B16A16_SFLOAT", vk::Format::eR16G16B16A16Sfloat},
-                                                               {"R16G16B16A16_SINT", vk::Format::eR16G16B16A16Sint},
-                                                               {"R16G16B16A16_UINT", vk::Format::eR16G16B16A16Uint},
-                                                               {"R32G32B32A32_SFLOAT", vk::Format::eR32G32B32A32Sfloat},
-                                                               {"R32G32B32A32_SINT", vk::Format::eR32G32B32A32Sint},
-                                                               {"R32G32B32A32_UINT", vk::Format::eR32G32B32A32Uint},
-                                                               {"B8G8R8A8_SRGB", vk::Format::eB8G8R8A8Srgb},
-                                                               {"B8G8R8A8_SINT", vk::Format::eB8G8R8A8Sint},
-                                                               {"B8G8R8A8_UINT", vk::Format::eB8G8R8A8Uint},
-                                                               {"B8G8R8A8_UNORM", vk::Format::eB8G8R8A8Unorm},
-                                                               {"B8G8R8A8_SNORM", vk::Format::eB8G8R8A8Snorm}};
+        static const astl::map<std::string, vk::Format> formatMap = {
+            {"R8G8B8A8_UNORM", vk::Format::eR8G8B8A8Unorm},
+            {"R8G8B8A8_SNORM", vk::Format::eR8G8B8A8Snorm},
+            {"R8G8B8A8_SRGB", vk::Format::eR8G8B8A8Srgb},
+            {"R8G8B8A8_SINT", vk::Format::eR8G8B8A8Sint},
+            {"R8G8B8A8_UINT", vk::Format::eR8G8B8A8Uint},
+            {"R16G16B16A16_SFLOAT", vk::Format::eR16G16B16A16Sfloat},
+            {"R16G16B16A16_SINT", vk::Format::eR16G16B16A16Sint},
+            {"R16G16B16A16_UINT", vk::Format::eR16G16B16A16Uint},
+            {"R32G32B32A32_SFLOAT", vk::Format::eR32G32B32A32Sfloat},
+            {"R32G32B32A32_SINT", vk::Format::eR32G32B32A32Sint},
+            {"R32G32B32A32_UINT", vk::Format::eR32G32B32A32Uint},
+            {"B8G8R8A8_SRGB", vk::Format::eB8G8R8A8Srgb},
+            {"B8G8R8A8_SINT", vk::Format::eB8G8R8A8Sint},
+            {"B8G8R8A8_UINT", vk::Format::eB8G8R8A8Uint},
+            {"B8G8R8A8_UNORM", vk::Format::eB8G8R8A8Unorm},
+            {"B8G8R8A8_SNORM", vk::Format::eB8G8R8A8Snorm}};
         std::transform(str.begin(), str.end(), str.begin(), ::toupper);
         auto it = formatMap.find(str);
         if (it == formatMap.end()) throw std::runtime_error("Unknown format: " + str);
