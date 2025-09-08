14437ae43802e99e273f5bf9013e99dc0640ae7e
Wusiki Jeronii <wusikijeronii@gmail.com>
2025-09-08 17:06:30 +0300

    setup.sh

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+git submodule update --init
+
+cd modules/umbf/
+git submodule update --init -- modules/3rd-party/rectpack2D
+cd - >/dev/null
+
+cd modules/aecl/
+git submodule update --init -- modules/3rd-party/earcut/
+cd - >/dev/null
+
+echo "Submodules initialized."
